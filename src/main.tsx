c46968535285e177555db11fb78312f4fbe8ba6b
Fabian Ruiz <fabian.ruizseiler@gmail.com>
2025-05-26 10:40:36 +0200

    Refactor: Wrap App with AuthProvider in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,14 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
 import './index.css'
+import { AuthProvider } from '@/contexts/AuthContext'
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <AuthProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </AuthProvider>,
+)
