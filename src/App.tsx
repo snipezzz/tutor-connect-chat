3a4eae14e6ec805c7c7f0a0a967bf7751b60a6f3
snipezzz <fabian.ruizseiler@gmail.com>
2025-05-26 14:05:49 +0200

    Bug Fix

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { Routes, Route } from 'react-router-dom'
 import Index from './pages/Index'
 import Auth from './pages/Auth'
 import NotFound from './pages/NotFound'
+import { AuthProvider } from '@/providers/AuthProvider';
 
 const queryClient = new QueryClient()
 
@@ -15,11 +16,13 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/auth" element={<Auth />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </AuthProvider>
     </TooltipProvider>
   </QueryClientProvider>
 )
