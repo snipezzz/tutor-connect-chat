0fa7e61db5f1e06fd08e788cd3999a2e9df8bace
snipezzz <fabian.ruizseiler@gmail.com>
2025-05-26 14:10:40 +0200

    Login Bug Fix

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -10,15 +10,22 @@ if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
   console.error('Supabase URL oder Anon Key ist nicht gesetzt.')
 }
 
-export const supabase = createClient<Database>(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY,
-  {
-    auth: {
-      // Session-Token im localStorage/cookie behalten
-      persistSession: true,
-      // Verhindert, dass Supabase URL-Parameter anpasst (optional)
-      detectSessionInUrl: false
-    }
+let supabaseInstance: ReturnType<typeof createClient<Database>> | null = null;
+
+export const supabase = () => {
+  if (!supabaseInstance) {
+    supabaseInstance = createClient<Database>(
+      SUPABASE_URL,
+      SUPABASE_ANON_KEY,
+      {
+        auth: {
+          // Session-Token im localStorage/cookie behalten
+          persistSession: true,
+          // Verhindert, dass Supabase URL-Parameter anpasst (optional)
+          detectSessionInUrl: false
+        }
+      }
+    );
   }
-)
+  return supabaseInstance;
+};
